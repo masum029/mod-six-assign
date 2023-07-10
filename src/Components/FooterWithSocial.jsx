198da778eb7bda23099b402fa46e6475215519a5
Md Abdullah Al Masum <52232643+masum029@users.noreply.github.com>
2023-07-10 18:03:09 +0600

    last commit

---
+++ src/Components/FooterWithSocial.jsx
@@ -0,0 +1,45 @@
+
+
+const FooterSocial=()=>{
+    return(
+<footer class="footer-section bg-light">
+        <div class="container">
+            <div class="footer-cta pt-5 pb-5 ">
+                <div class="row">
+                    <div class="col-xl-4 col-md-4 mb-30">
+                        <div class="single-cta">
+                            <i class="fas fa-map-marker-alt"></i>
+                            <div class="cta-text">
+                                <h4>Find me</h4>
+                                <span>Sector-3, Uttara, Dhaka-1230, Bangladesh</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-md-4 mb-30">
+                        <div class="single-cta">
+                            <i class="fas fa-phone"></i>
+                            <div class="cta-text">
+                                <h4>Direct Contact</h4>
+                                <span>+8801766070988</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-md-4 mb-30">
+                        <div class="single-cta">
+                            <i class="far fa-envelope-open"></i>
+                            <div class="cta-text">
+                                <h4>Contact via Mail</h4>
+                                <span>masum06aiub@gmail.com</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        
+    </footer>
+    );
+}
+
+export default FooterSocial
---
+++ src/Components/TextboxValue.jsx
@@ -0,0 +1,65 @@
+
+import React, { useRef,useState } from 'react';
+
+
+function MyComponent() {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Title</h1>
+      <input type="text" value={inputValue} onChange={handleInputChange} />
+      <p>You entered: {inputValue}</p>
+    </div>
+  );
+}
+
+
+// Using a class component and handling state:
+
+class MyComponent3 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: ''
+    };
+  }
+
+  handleInputChange = (event) => {
+    this.setState({ inputValue: event.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Title</h1>
+        <input type="text" value={this.state.inputValue} onChange={this.handleInputChange} />
+        <p>You entered: {this.state.inputValue}</p>
+      </div>
+    );
+  }
+}
+
+
+
+
+function MyComponent2() {
+  const inputRef = useRef(null);
+
+  const handleButtonClick = () => {
+    const value = inputRef.current.value;
+    console.log('Input value:', value);
+  };
+
+  return (
+    <div>
+      <h1>Title</h1>
+      <input type="text" ref={inputRef} />
+      <button onClick={handleButtonClick}>Display Value</button>
+    </div>
+  );
+}
