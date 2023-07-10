37292afa34123c8a87fa219cf52aa5ee341698a2
Md Abdullah Al Masum <52232643+masum029@users.noreply.github.com>
2023-07-10 17:34:24 +0600

    btn function add

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,24 +2,41 @@
 import React from "react";
 import Marque from "./Marque";
 import FooterSlider from "./Carousel";
+import FooterSocial from "./FooterWithSocial";
+import MyImage from "../Images/2.jpg"
 
 
 
-const marqueeData=["Read, ˹O Prophet,˺ in the Name of your Lord Who created.----","“O, happy the soul that saw its own faults.” Rumi--------","A self-absorbed person only can see the faults of others but they are often color blind to their own.---------","“Before raise any question about others make sure that you are perfect.” aam"];
+const marqueeData=["Read, ˹O Prophet,˺ in the Name of your Lord Who created."];
 const Footer = (props) => {
   return (
     <>
-    <footer>
+  <hr className="featurette-divider"></hr>
+<div className="row featurette">
+      <div className="col-md-7 mr">
+        <h2 className="featurette-heading">First featurette heading. <span className="text-muted">It’ll blow your mind.</span></h2>
+        <p className="lead">I may not be there yet, but I am closer than I was yesterday.</p>
+      </div>
+      <div className="col-md-5">
+        {/* <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg> */}
+
+        <img src={MyImage} className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"  width="500" height="500" alt="I may not be there yet, but I am closer than I was yesterday" />
+    
+      </div>
+    </div>
+    <footer className="bg-dark">
         
-        <marquee behavior="scroll" direction="left">
+
+
+        <marquee className="text-info" behavior="scroll" direction="left">
             {marqueeData.map((item, index) => (
-            <span key={index}>{index+'-'}{item}</span>
+            <span key={index}>{item}</span>
             ))}
         </marquee>
+        <FooterSocial/>
+        {/* <FooterSlider/> */}
 
-        <FooterSlider/>
-
-        <p>&copy; {new Date().getFullYear()} Design and Developed by <strong>aam</strong> </p>
+        {/* <p>&copy; {new Date().getFullYear()} Design and Developed by <strong>aam</strong> </p> */}
     </footer>    
     </>
   );
